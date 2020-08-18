be0920b476ccdaa5c89ad7515e105f705d348058
JINYU-FAN <1919304560@qq.com>
2020-08-18 09:38:05 +0800

    PoissonNeuron finished

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from torch_snn.F import update
-from torch_snn.neuron import LinearNeuron, InputNeuron, LIFNeuron
+from torch_snn.neuron import LinearNeuron, InputNeuron, LIFNeuron, PoissonNeuron
 from torch_snn.synapse import FixedSynapse, STDPSynapse
 from torch_snn.monitor import VoltageMonitor, SpikeMonitor, SynapseMonitor
 import torch
 import copy
-
+'''
 n1 = InputNeuron(2)
 
 n1.input([[0,0,0,0,0],[1,0,1,0,0]])
@@ -17,10 +17,13 @@ m1 = SynapseMonitor(s)
 m2 = SpikeMonitor(n1)
 m3 = SpikeMonitor(n2)
 
-for i in range(2000): 
+'''
+n3 = PoissonNeuron(5)
+m4 = SpikeMonitor(n3)
+for i in range(100): 
     update()
-
+print(n3.image)
 #m2.plot()
 #m3.plot()
-m1.plot()
+m4.plot()
 
